55942b1c7f9da4c63e30e36182bc484f5efe401f
Aviral Srivastava <srivastavaaviral1@gmail.com>
2017-11-01 16:22:27 +0530

    setting up client.js and app.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,10 @@
+// var React = require{'react'}
+import React from 'react'
+// var render = require{'react-dom'}.render;
+import {render} from 'react-dom'
+import App from '../component/App'
 
+render(
+        <App/>,
+        document.getElementById('app')
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,10 @@
+import React, {Components} from 'react'
 
+class App extends Component {
+
+    render(){
+        return <div>Check if React works!!</div>
+    }
+}
+
+export default App

45a4ff5fd0af2ae35d28d34e3c9cdcd4f655a925
Aviral Srivastava <srivastavaaviral1@gmail.com>
2017-11-02 16:24:19 +0530

    creating input box and resolving the getInitialState error

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react'
 
 class TextDisplay extends Component{
+
+    // getInitialState(){
+    //     return {
+    //         inputText: 'initial text'
+    //     }
+    // }
+    constructor(props,context){
+        super(props, context)
+        this.state={
+            inputText: 'initial txt'
+        }
+    }
     render(){
-        return <div>text display</div>
+        return <input type="text" placeholder="Enter your Task here" value={this.state.inputText} />
     }
 }
 

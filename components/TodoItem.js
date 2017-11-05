36937c3992feb62acb53cbed9774e8c12042513c
Aviral Srivastava <srivastavaaviral1@gmail.com>
2017-11-05 15:08:35 +0530

    added a css line-through for completed tasks

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import actions from '../redux/actions'
 
 class TodoiItem extends Component{
+    
 
     handleComplete(){
             this.props.dispatch(actions.completeTodo(this.props.todo.id))
@@ -11,11 +12,12 @@ class TodoiItem extends Component{
         this.props.dispatch(actions.deleteTodo(this.props.todo.id))
     }
 
+
     render(){
         return (
             <li>
-                <input type="checkbox" onClick={this.handleComplete.bind(this)} />
-               <span> {this.props.todo.text} </span>
+                <input type="checkbox" onChange={this.handleComplete.bind(this)} />
+               <span  className={this.props.todo.completed ? 'completed' : 'none' }> { this.props.todo.text} </span>
               <button onClick={this.handleDelete.bind(this)} >&#x2718;</button>
                {/* <i className="fa fa-trash" aria-hidden="true" onClick={this.handleDelete.bind(this) > */}
             </li>

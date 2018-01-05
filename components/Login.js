4fa2661bcb51450e417845ab601b1fe3b2e7a7aa
Aviral Srivastava <srivastavaaviral1@gmail.com>
2018-01-05 23:42:55 +0530

    build validation in sign up form

--- components/Login.js
+++ components/Login.js
@@ -1,127 +1,125 @@
-    import React, {Component} from 'react';
-    import {browserHistory} from 'react-router-dom';
-    import { Route , withRouter} from 'react-router-dom';
-    import {Redirect} from 'react-router-dom'
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
+import { Redirect } from 'react-router-dom'
 
-    class Login extends Component {
+class Login extends Component {
 
-        constructor(props){
-            super(props);
-            this.state ={
-                username: "",
-                password: "",
-                redirect: false
-            }
-            this.login = this.login.bind(this);
-            this.change = this.change.bind(this);
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            redirect: false
         }
+        this.login = this.login.bind(this);
+        this.change = this.change.bind(this);
+    }
 
 
-        change(e){
-            // console.log("onChange working");
-            this.setState({[e.target.name]: e.target.value});
-            this.showInputError(e.target.name);
+    change(e) {
+        // console.log("onChange working");
+        this.setState({ [e.target.name]: e.target.value });
+        this.showInputError(e.target.name);
+    }
+
+    login(e) {
+        e.preventDefault();
+        // console.log("you clicked login button");
+        if (!this.validate()) {
+            console.log("empty fields");
+
         }
-
-        login(e){
-            e.preventDefault();
-            // console.log("you clicked login button");
-            if(!this.validate()){
-                console.log("empty fields");
-               
-            }
-            else{
-                this.setState({redirect:true});
-                // this.setState({
-                //     password : this.state.passwordError
-                //   });
-            }
-        }
-
-        validate = () => {
-           
-            const inputs = document.querySelectorAll('input');
-            let isFormValid = true;
-
-
-            // if (this.state.username.length < 5) {
-            //   isError = true;
-            //   errors.usernameError = "Username needs to be atleast 5 characters long";
-            // }
-      
-            // if (this.state.password.length <= 0) {
-            //   isError = true;
-            //   errors.passwordError = "Requires valid password";
-            // }
-            inputs.forEach(input => {
-               
-                const isInputValid = this.showInputError(input.name);
-                
-                if (!isInputValid) {
-                  isFormValid = false;
-                }
-              });
-      
-      
-            return isFormValid;
-          };
-
-          showInputError(refName) {
-            const validity = this.refs[refName].validity;
-            const label = document.getElementById(`${refName}Label`).textContent;
-            const error = document.getElementById(`${refName}Error`);
-            const isPassword = refName.indexOf('password') !== -1;
-           
-                
-            if (!validity.valid) {
-              if (validity.valueMissing) {
-                error.textContent = `${label} is a required field`; 
-              } else if (validity.typeMismatch) {
-                error.textContent = `${label} should be a valid email address`; 
-              } else if (isPassword && validity.patternMismatch) {
-                error.textContent = `${label} should be longer than 4 chars`; 
-              } 
-              return false;
-            }
-            
-            error.textContent = '';
-            return true;
-          }
-
-
-        render() {
-
-            if(this.state.redirect){
-                return (<Redirect to={'/main'} />)
-            }
-            return (
-            <div className="form">
-                <h2>Login</h2>
-                <form>
-            
-            <div className="group">
-        <input type="text"   name="username"  ref="username"
-                                onChange={e => this.change(e)}
-                                required="required"/><span className="highlight"></span><span className="bar"></span>
-        <label id="usernameLabel">Username</label>
-        <div className="error" id="usernameError" />
-        </div>
-        <div className="group">
-        <input type="password"  name="password"  ref="password"
-                                ref="password" pattern=".{5,}"
-                                value={ this.state.password }
-                                onChange={e => this.change(e)}
-                                required="required"/><span className="highlight"></span><span className="bar"></span>
-        <label id="passwordLabel">Password</label>
-        <div className="error" id="passwordError" />
-        </div>
-        <div className="btn-box">
-        <button className="button btn-submit"  onClick={e => this.login(e)} type="submit">login</button>
-        </div>
-                </form>
-            </div>
-            )
+        else {
+            this.setState({ redirect: true });
+            // this.setState({
+            //     password : this.state.passwordError
+            //   });
         }
     }
 
-    export default Login;
+    validate = () => {
+
+        const inputs = document.querySelectorAll('input');
+        let isFormValid = true;
+
+
+        // if (this.state.username.length < 5) {
+        //   isError = true;
+        //   errors.usernameError = "Username needs to be atleast 5 characters long";
+        // }
+
+        // if (this.state.password.length <= 0) {
+        //   isError = true;
+        //   errors.passwordError = "Requires valid password";
+        // }
+        inputs.forEach(input => {
+
+            const isInputValid = this.showInputError(input.name);
+
+            if (!isInputValid) {
+                isFormValid = false;
+            }
+        });
+
+
+        return isFormValid;
+    };
+
+    showInputError(refName) {
+        const validity = this.refs[refName].validity;
+        const label = document.getElementById(`${refName}Label`).textContent;
+        const error = document.getElementById(`${refName}Error`);
+        const isPassword = refName.indexOf('password') !== -1;
+
+
+        if (!validity.valid) {
+            if (validity.valueMissing) {
+                error.textContent = `${label} is a required field`;
+            } else if (isPassword && validity.patternMismatch) {
+                error.textContent = `${label} should be longer than 4 characters`;
+            }
+            return false;
+        }
+
+        error.textContent = '';
+        return true;
+    }
+
+
+    render() {
+
+        if (this.state.redirect) {
+            return (<Redirect to={'/main'} />)
+        }
+        return (
+            <div className="form">
+                <h2>Login</h2>
+                <form>
+
+                    <div className="group">
+                        <input type="text" name="username" ref="username"
+                            onChange={e => this.change(e)}
+                            required="required" /><span className="highlight"></span><span className="bar"></span>
+                        <label id="usernameLabel">Username</label>
+                        <div className="error" id="usernameError" />
+                    </div>
+                    <div className="group">
+                        <input type="password" name="password" ref="password"
+                            pattern=".{5,}"
+                            value={this.state.password}
+                            onChange={e => this.change(e)}
+                            required="required" /><span className="highlight"></span><span className="bar"></span>
+                        <label id="passwordLabel">Password</label>
+                        <div className="error" id="passwordError" />
+                    </div>
+                    <div className="btn-box">
+                        <button className="button btn-submit" onClick={e => this.login(e)} type="submit">login</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,202 +1,197 @@
-  import React, {Component} from 'react';
-  import {browserHistory} from 'react-router-dom';
-  import { Route , withRouter} from 'react-router-dom';
-  import { FacebookLogin } from 'react-facebook-login-component';
+    import React, {Component} from 'react';
+    import {browserHistory} from 'react-router-dom';
+    import { Route , withRouter} from 'react-router-dom';
+    import { FacebookLogin } from 'react-facebook-login-component';
 
-  class SignUp extends Component {
-  constructor(props){
-      super(props);
-      this.state ={
-          // email: '',
-          // password: '',
-          // error:{
-          //     message: ''
-          // }
-          firstName: "",
-          firstNameError: "",
-          lastName: "",
-          lastNameError: "",
-          username: "",
-          usernameError: "",
-          email: "",
-          emailError: "",
-          password: "",
-          passwordError: ""
-      }
-      this.onSubmit = this.onSubmit.bind(this);
-      this.change = this.change.bind(this);
-  }
+    class SignUp extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            // email: '',
+            // password: '',
+            // error:{
+            //     message: ''
+            // }
+            firstname: "",
+            lastname: "",
+            username: "",
+            email: "",
+            password: "",
+        }
+        this.onSubmit = this.onSubmit.bind(this);
+        this.change = this.change.bind(this);
+    }
 
 
 
-  // change(e) {
-  //     // this.props.onChange({ [e.target.name]: e.target.value });
-  //     this.setState({
-  //       [e.target.name]: e.target.value
-  //     });
-  //   };
-  change = e => {
-      // this.props.onChange({ [e.target.name]: e.target.value });
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    };
-
-    validate = () => {
-      let isError = false;
-      const errors = {
-        firstNameError: "",
-        lastNameError: "",
-        usernameError: "",
-        emailError: "",
-        passwordError: ""
+    // change(e) {
+    //     // this.props.onChange({ [e.target.name]: e.target.value });
+    //     this.setState({
+    //       [e.target.name]: e.target.value
+    //     });
+    //   };
+    change = e => {
+        // this.props.onChange({ [e.target.name]: e.target.value });
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+        this.showInputError(e.target.name);
       };
 
-      if (this.state.username.length < 5) {
-        isError = true;
-        errors.usernameError = "Username needs to be atleast 5 characters long";
-      }
 
-      if (this.state.email.indexOf("@") === -1) {
-        isError = true;
-        errors.emailError = "Requires valid email";
-      }
-
-      this.setState({
-          ...this.state,
-          ...errors
-        });
-
-      return isError;
-    };
-
-  
-  
-    onSubmit = e => {
-      e.preventDefault();
-      // this.props.onSubmit(this.state);
-      // console.log('submit is clicked');
-      const err = this.validate();
-      if (!err) {
-        // clear form
-        this.setState({
-          firstName: "",
-          firstNameError: "",
-          lastName: "",
-          lastNameError: "",
-          username: "",
-          usernameError: "",
-          email: "",
-          emailError: "",
-          password: "",
-          passwordError: ""
-        });
-        this.props.history.push('/main');
-        console.log(this.props);
-        // this.props.onChange({
-        //   firstName: "",
-        //   lastName: "",
-        //   username: "",
-        //   email: "",
-        //   password: ""
-        // });
-        console.log('no errors');
-      }
-      else{
+      onSubmit = e => {
+        e.preventDefault();
+        // this.props.onSubmit(this.state);
+        // console.log('submit is clicked');
+        if (!this.validate()) {
           console.log('This form has errors');
-          console.log(this);
+          // console.log(this);
           // console.log(this.state.errorText);
           // console.log(this.state.value);
           //  console.log(e.target);
-          console.log(this.props);
+          // console.log(this.props);
       //     // this.setState({
       //     //  email : this.state.emailError
       //     // });
-          this.setState({
-            email : this.state.emailError
-          });
-       }
-    };
-
-
-    responseFacebook (response) {
-      console.log(response);
-      //anything else you want to do(save to localStorage)...
-    }
-    
-    render() {
-          return (
-            <div>
+          // this.setState({
+          //   email : this.state.emailError
+          // });
         
-          <div className="wrapper">
-            <form>
-            
-            <div className="group">
-        <input type="text"   name="firstName"
-                              value={this.state.firstName}
-                              onChange={e => this.change(e)}
-                              // onChange={this.change.bind(this)}
-                              errorText={this.state.firstNameError}
-                              required="required"/><span className="highlight"></span><span className="bar"></span>
-        <label> First Name</label>
-      </div>
-        <div className="group">
-          <input type="text" name="lastName"
-                                value={this.state.lastName}
+        }
+        else{
+            // clear form
+          this.setState({
+            firstname: "",
+            lastname: "",
+            username: "",
+            email: "",
+            password: "",
+          });
+          this.props.history.push('/main');
+          // console.log(this.props);
+          console.log('no errors');
+        }
+      };
+
+
+      validate = () => {
+
+        const inputs = document.querySelectorAll('input');
+        let isFormValid = true;
+
+        inputs.forEach(input => {
+          
+                      const isInputValid = this.showInputError(input.name);
+          
+                      if (!isInputValid) {
+                          isFormValid = false;
+                      }
+                  });
+          
+          
+                  return isFormValid;
+              };
+
+              showInputError(refName) {
+                const validity = this.refs[refName].validity;
+                const label = document.getElementById(`${refName}Label`).textContent;
+                const error = document.getElementById(`${refName}Error`);
+                const isPassword = refName.indexOf('password') !== -1;
+        
+        
+                if (!validity.valid) {
+                    if (validity.valueMissing) {
+                        error.textContent = `${label} is a required field`;
+                    } else if (isPassword && validity.patternMismatch) {
+                        error.textContent = `${label} should be longer than 4 characters`;
+                    } else if (validity.typeMismatch) {
+                      error.textContent = `${label} is not a valid email address`;
+                  }
+                    return false;
+                }
+        
+                error.textContent = '';
+                return true;
+            }
+
+     
+      responseFacebook (response) {
+        console.log(response);
+        //anything else you want to do(save to localStorage)...
+      }
+      
+      render() {
+            return (
+              <div>
+          
+            <div className="wrapper">
+              <form>
+              
+              <div className="group">
+          <input type="text"   name="firstname" ref="firstname"
+                                value={this.state.firstName}
                                 onChange={e => this.change(e)}
                                 // onChange={this.change.bind(this)}
-                                errorText={this.state.lastNameError}
                                 required="required"/><span className="highlight"></span><span className="bar"></span>
-          <label>Last Name</label>
+          <label id="firstnameLabel"> First Name</label>
+          <div className="error" id="firstnameError" />
         </div>
-      <div className="group">
-        <input type="text"   name="username"
-                              value={this.state.username}
-                              onChange={e => this.change(e)}
-                              // onChange={this.change.bind(this)}
-                              errorText={this.state.usernameError}
-                              required="required"/><span className="highlight"></span><span className="bar"></span>
-        <label>Username</label>
+          <div className="group">
+            <input type="text" name="lastname" ref="lastname"
+                                  value={this.state.lastName}
+                                  onChange={e => this.change(e)}
+                                  // onChange={this.change.bind(this)}
+                                  required="required"/><span className="highlight"></span><span className="bar"></span>
+            <label id="lastnameLabel">Last Name</label>
+            <div className="error" id="lastnameError" />
+          </div>
+          <div className="group">
+          <input type="text" name="username" ref="username"
+          value={this.state.username}
+              onChange={e => this.change(e)}
+              required="required" /><span className="highlight"></span><span className="bar"></span>
+          <label id="usernameLabel">Username</label>
+          <div className="error" id="usernameError" />
       </div>
-      <div className="group">
-        <input type="text"   name="email"
-                              value={this.state.email}
-                              onChange={e => this.change(e)}
-                              // onChange={this.change.bind(this)}
-                              errorText={this.state.emailError}
-                              required="required"/><span className="highlight"></span><span className="bar"></span>
-        <label>Email</label>
-      </div>
-      <div className="group">
-        <input type="password"  name="password"
-                              value={this.state.password}
-                              onChange={e => this.change(e)}
-                              // onChange={this.change.bind(this)}
-                              errorText={this.state.passwordError}
-                              required="required"/><span className="highlight"></span><span className="bar"></span>
-        <label>Password</label>
-      </div>
-      
-      <div className="btn-box">
-        <button className="button btn-submit"  onClick={e => this.onSubmit(e)} type="submit">Sign Up</button>
-        <FacebookLogin  socialId="158761201565558"
-                            language="en_US"
-                            scope="public_profile,email"
-                            responseHandler={this.responseFacebook}
-                            xfbml={true}
-                            fields="id,email,name"
-                            version="v2.5"
-                            className="button btn-submit facebook-login"
-                            buttonText="Login With Facebook"/>
+        <div className="group">
+          <input type="email"   name="email" ref="email"
+                                value={this.state.email}
+                                onChange={e => this.change(e)}
+                                // onChange={this.change.bind(this)}
+                                required="required"/><span className="highlight"></span><span className="bar"></span>
+          <label id="emailLabel">Email</label>
+          <div className="error" id="emailError" />
         </div>
-        <h4>Sign Up</h4>
-          <p>Already registered? <a href="/login">Sign in</a></p>
-              </form>
-              {/* <div>{this.state.error.message}</div> */}
+        <div className="group">
+        <input type="password" name="password" ref="password"
+            pattern=".{5,}"
+            value={this.state.password}
+            onChange={e => this.change(e)}
+            required="required" /><span className="highlight"></span><span className="bar"></span>
+        <label id="passwordLabel">Password</label>
+        <div className="error" id="passwordError" />
+    </div>
+        
+        <div className="btn-box">
+          <button className="button btn-submit"  onClick={e => this.onSubmit(e)} type="submit">Sign Up</button>
+          <FacebookLogin  socialId="158761201565558"
+                              language="en_US"
+                              scope="public_profile,email"
+                              responseHandler={this.responseFacebook}
+                              xfbml={true}
+                              fields="id,email,name"
+                              version="v2.5"
+                              className="button btn-submit facebook-login"
+                              buttonText="Login With Facebook"/>
           </div>
-          </div>
-          )
-      }
-  }
+          <h4>Sign Up</h4>
+            <p>Already registered? <a href="/login">Sign in</a></p>
+                </form>
+                {/* <div>{this.state.error.message}</div> */}
+            </div>
+            </div>
+            )
+        }
+    }
 
-  export default SignUp;
+    export default SignUp;

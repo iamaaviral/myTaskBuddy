6a290e5930fc7fb5472b883b81bdd7f26fc72ace
srivastavaaviral1 <srivastavaaviral1@gmail.com>
2018-01-18 23:59:12 +0530

    corrected the bug in previous commit in delete completed and important todo

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,29 +7,6 @@ function getId(state){
 let reducer = function(state, action){
     switch (action.type) {
         case 'ADD_TODO':
-                //  console.log("this.props.dispatch error");
-                //Have used ES6 syntax object.assign
-                //     return Object.assign({},state,{
-                //     // todos: [{
-                //     //     //add new todo info
-                //     // text: action.text,
-                //     // completed: false,
-                //     // important: false,
-                //     // id: getId(state)
-                //     // }, ...state.todos]
-                //     Todo: [{
-                //         todos: [{
-                //                 //add new todo info
-                //             text: action.text,
-                //             completed: false,
-                //             important: false,
-                //             id: getId(state)
-                //             }, ...state.Todo[state.selectedCategory].todos]
-                //     }, ...state.Todo]
-                // })
-                // console.log(state);
-                
-                // console.log(state.Todo);
                 var item = {text: action.text,
                                 completed: false,
                                 important: false,
@@ -40,40 +17,40 @@ let reducer = function(state, action){
                 var allItems = state.Todo[state.selectedCategory].todos.concat([item]);
                 // console.log(allItems);
                 newtodo[state.selectedCategory].todos = allItems;
-                // console.log( newtodo[state.selectedCategory].todos);
-                // console.log(newtodo);
-                // setState({
-                //     Todo: newtodo
-                // });
-                // console.log(state.Todo);
                 return Object.assign({},state, {
                      state : {newtodo, ...state }
                 })
         case 'COMPLETE_TODO':
-                    return Object.assign({},state, {
-                       Todo:[{
-                        todos: state.Todo[state.selectedCategory].todos.map((todo) => {
+                        var newtodo = state.Todo;
+                        var allItems = state.Todo[state.selectedCategory].todos.map((todo) => {
                             return todo.id === action.id ? Object.assign({}, todo, {completed : !todo.completed}) : todo
-                        })
-                       }]
+                        });
+                        newtodo[state.selectedCategory].todos = allItems;
+
+                        return Object.assign({},state, {
+                            state : {newtodo, ...state }
                     })
         
-        
           case 'IMPORTANT_TODO':
-                    return Object.assign({},state, {
-                        Todo:[{
-                            todos: state.Todo[state.selectedCategory].todos.map((todo) => {
-                                return todo.id === action.id ? Object.assign({}, todo, {important : !todo.important}) : todo
-                            })
-                         }]
+                        var newtodo = state.Todo;
+                        var allItems = state.Todo[state.selectedCategory].todos.map((todo) => {
+                            return todo.id === action.id ? Object.assign({}, todo, {important : !todo.important}) : todo
+                        });
+                        newtodo[state.selectedCategory].todos = allItems;
+
+                        return Object.assign({},state, {
+                            state : {newtodo, ...state }
                     })
 
         case 'DELETE_TODO':
-                    return Object.assign({},state, {
-                       Todo:[{ todos: state.Todo[state.selectedCategory].todos.filter((todo) => {
+                        var newtodo = state.Todo;
+                        var allItems = state.Todo[state.selectedCategory].todos.filter((todo) => {
                             return todo.id !== action.id
-                            })
-                        }]
+                        })
+                        newtodo[state.selectedCategory].todos = allItems;
+
+                        return Object.assign({},state, {
+                            state : {newtodo, ...state }
                     })
 
         default: 

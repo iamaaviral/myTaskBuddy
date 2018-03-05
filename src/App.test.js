03fec21b1bd5ae04eadb72b855957394e2dfcc52
srivastavaaviral1 <srivastavaaviral1@gmail.com>
2018-03-05 13:13:53 +0530

    adding a test

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,7 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import { configure } from 'enzyme';
 import App from './components/App';
+import SignUp from './components/SignUp';
 import Adapter from 'enzyme-adapter-react-16';
 // import TestUtils from 'react-addons-test-utils'
 // import ReactTestUtils from 'react-dom/test-utils'
@@ -18,6 +19,6 @@ configure({ adapter: new Adapter() });
 // })
 
 test('App component should render as expected', () => {
-        const component = shallow(<App />).dive();
-        console.log(component);
+        const component = shallow(<SignUp />);
+        expect(component.contains('hey!!')).toBe(true);
 })
